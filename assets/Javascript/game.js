28c547e33ce835ed860306d3478068ed6e8271de
olive <daniellek@mobilefuse.com>
2019-10-03 12:54:13 -0400

    image added

---
+++ assets/Javascript/game.js
@@ -0,0 +1,20 @@
+//array of all letters
+let letters = "abcdefghijklmnopqrstuvwxyz";
+
+
+//function onkeyup player letter & random letter generates. Both display on screen
+
+player.addEventListener("keydown", log);
+
+function log(event){
+  console.log( event.type );
+}
+
+
+//if else validation 
+//player selection
+//computer random letter
+
+
+//Match = score for the player
+//Each guess Guesses Left:incriment down by 1. Guesses so far go up
