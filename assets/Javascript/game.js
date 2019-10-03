f4d248f3d4604605fc505c2bc7e6c5b85c2654b3
olive <daniellek@mobilefuse.com>
2019-10-03 18:03:36 -0400

    js coding for computer and user guess, and logic for match vs noMatch

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -1,14 +1,9 @@
 //array of all letters
-let letters = "abcdefghijklmnopqrstuvwxyz";
+
 
 
 //function onkeyup player letter & random letter generates. Both display on screen
 
-player.addEventListener("keydown", log);
-
-function log(event){
-  console.log( event.type );
-}
 
 
 //if else validation 
@@ -18,3 +13,34 @@ function log(event){
 
 //Match = score for the player
 //Each guess Guesses Left:incriment down by 1. Guesses so far go up
+let letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+let match = 0;
+let noMatch = 0; 
+let guesses = 0;
+let guessesLeft = 0;
+
+//player guess 
+document.onkeyup = function () {
+  var playerGuess = String.fromCharCode(event.keyCode). //learned this from RPS vid
+    toLocaleLowerCase();
+
+    console.log(playerGuess);
+//computer guess
+    var computerGuess = letters[Math.floor(Math.random()*letters
+      .length)];
+
+    console.log(computerGuess);
+
+   //letter match alert
+    if ((playerGuess==computerGuess)){
+      match++;
+      alert('We are on the same wave length!!! '+ match);
+    }
+    //letter does not match
+    if ((playerGuess!=computerGuess)){
+      noMatch++;
+      alert('My Crystal Ball is cloudy!!! '+ noMatch);
+    }
+  }
+  
+
